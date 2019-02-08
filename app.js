28827b977cabae3ef630451f6271908f86b8ea08
Karol <karol.zieniewicz@gmail.com>
2019-02-08 18:42:11 +0100

    add letters generator in js

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-const BOARD = document.getElementById('letters');
+const gameWrapper = document.querySelector('.game-wrapper');
+gameSetup();
 
+function gameSetup() {
+  for (i = 0; i < 26; i++) {
+    let insertedDiv = document.createElement('div');
+    let paragraph = document.createElement('p');
+    let letter = String.fromCharCode(i + 97);
+    paragraph.innerHTML = letter;
+    insertedDiv.append(paragraph);
+    insertedDiv.classList.add('letter-wrapper', `${letter}`, 'border-all');
+    if (i === 0) {
+      insertedDiv.classList.add('top-left');
+    } else if (i > 0 && i < 6) {
+      insertedDiv.classList.add('top');
+    } else if (i === 6) {
+      insertedDiv.classList.add('top-right');
+    } else if (i > 6 && i < 13) {
+      insertedDiv.classList.add('right');
+    } else if (i === 13) {
+      insertedDiv.classList.add('bottom-right');
+    } else if (i > 13 && i < 19) {
+      insertedDiv.classList.add('bottom');
+    } else if (i === 19) {
+      insertedDiv.classList.add('bottom-left');
+    } else {
+      insertedDiv.classList.add('left');
+    }
+    gameWrapper.appendChild(insertedDiv);
+  }
+}
