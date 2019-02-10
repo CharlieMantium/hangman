611df1773b837fe276482a41f8aecfa0831b360e
Karol <karol.zieniewicz@gmail.com>
2019-02-10 19:15:08 +0100

    change after CR

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 const gameWrapper = document.querySelector('.game-wrapper');
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+
 gameSetup();
 
 function gameSetup() {
-  for (i = 0; i < 26; i++) {
+  alphabet.forEach((item, i) => {
     let insertedDiv = document.createElement('div');
     let paragraph = document.createElement('p');
     let letter = String.fromCharCode(i + 97);
     paragraph.innerHTML = letter;
     insertedDiv.append(paragraph);
-    insertedDiv.classList.add('letter-wrapper', `${letter}`, 'border-all');
-    if (i === 0) {
-      insertedDiv.classList.add('top-left');
-    } else if (i > 0 && i < 6) {
-      insertedDiv.classList.add('top');
-    } else if (i === 6) {
-      insertedDiv.classList.add('top-right');
-    } else if (i > 6 && i < 13) {
-      insertedDiv.classList.add('right');
-    } else if (i === 13) {
-      insertedDiv.classList.add('bottom-right');
-    } else if (i > 13 && i < 19) {
-      insertedDiv.classList.add('bottom');
-    } else if (i === 19) {
-      insertedDiv.classList.add('bottom-left');
-    } else {
-      insertedDiv.classList.add('left');
-    }
+    addLetterClasses(insertedDiv, letter, i);
     gameWrapper.appendChild(insertedDiv);
+  })
+}
+
+function addLetterClasses(letterDiv, letter, i) {
+  letterDiv.classList.add('letter-wrapper', `${letter}`);
+  if (i === 0) {
+    letterDiv.classList.add('top-left');
+  } else if (i > 0 && i < 6) {
+    letterDiv.classList.add('top');
+  } else if (i === 6) {
+    letterDiv.classList.add('top-right');
+  } else if (i > 6 && i < 13) {
+    letterDiv.classList.add('right');
+  } else if (i === 13) {
+    letterDiv.classList.add('bottom-right');
+  } else if (i > 13 && i < 19) {
+    letterDiv.classList.add('bottom');
+  } else if (i === 19) {
+    letterDiv.classList.add('bottom-left');
+  } else {
+    letterDiv.classList.add('left');
   }
+  return letterDiv;
 }
