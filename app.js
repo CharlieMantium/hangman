c5f43d01ad3e790bfc5ab4ce68a2b9a3c27ac425
Karol <karol.zieniewicz@gmail.com>
2019-02-12 17:18:03 +0100

    restored - letter click handeled

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const alphabet = [...'abcdefghijklmnopqrstuvwxyz']
 const mainBtn = document.querySelector('.btn');
 const imageWrapper = document.querySelector('.image-wrapper');
 const sentence = document.querySelector('.sentence');
-const sentences = [
+let roundCounter = 1;
+const proverbs = [
   'Grass is always greener on the other side',
   'It takes two to tango',
   'Money makes the world go round'
@@ -50,23 +51,27 @@ function gameSetup() {
   const letters = document.querySelectorAll('.letter-wrapper');
   [...letters].forEach(item => item.addEventListener('click', function(item) {
     let pickedLetter = item.target.innerHTML;
-    console.log(pickedLetter);
+    if (pickedLetter.charAt(0) === '<') {
+      letterCheck(pickedLetter.charAt(3));
+    } else {
+      letterCheck(pickedLetter);
+    }
   }));
   imageWrapper.innerHTML = '';
   let insertedImg = document.createElement('img');
   insertedImg.classList.add('image');
   insertedImg.src = 'img/s0.jpg';
   imageWrapper.appendChild(insertedImg);
-  sentence.innerHTML = '';
-  sentence.innerHTML = encryptSentence(sentences[getRandomInt(sentences.length)]);
+  let rndPickedProverb = proverbs[getRandomInt(proverbs.length)];
+  sentence.innerHTML = encryptSentence(rndPickedProverb);
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function encryptSentence(proverb) {
-  let encryptedArray = [...proverb].map(letter => {
+function encryptSentence(inputSentence) {
+  let encryptedArray = [...inputSentence].map(letter => {
     if (letter === ' ') {
       encryptedLetter = '-';
     } else {
@@ -75,4 +80,8 @@ function encryptSentence(proverb) {
     return encryptedLetter;
   })
   return encryptedArray.join('');
+}
+
+letterCheck() {
+  // to be continued...
 }
