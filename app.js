5a2af7a6e5e8bdc6808c8f39a3a074df47d6ed6d
Karol <karol.zieniewicz@gmail.com>
2019-03-01 09:53:20 +0100

    changed after CR

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const mainBtn = document.querySelector('.btn');
 const imageWrapper = document.querySelector('.image-wrapper');
 const sentence = document.querySelector('.sentence');
 const state = {
-  roundCounter: 0
+  roundCounter: 0,
+  proverb: '',
+  correct: [],
+  wrong: []
 };
 const proverbs = [
   'Grass is always greener on the other side',
@@ -54,16 +57,19 @@ function gameSetup() {
   addLetterListeners();
   insertNextImg();
   state.proverb = proverbs[getRandomInt(proverbs.length)].toLowerCase();
-  state.displayedProverb = encryptSentence(state.proverb);
-  sentence.innerHTML = state.displayedProverb;
+  displayProgress();
 }
 
-function addLetterListeners() {
+function displayProgress(){
+  sentence.innerHTML = encryptProverb();
+}
+
+function addLetterListeners(){
   const letters = document.querySelectorAll('.letter-wrapper');
   [...letters].forEach(item => item.addEventListener('click', () => letterCheck(item.id)));
 }
 
-function insertNextImg() {
+function insertNextImg(){
   let insertedImg = document.createElement('img');
   insertedImg.classList.add('image');
   insertedImg.src = `img/s${state.roundCounter}.jpg`;
@@ -75,24 +81,19 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function encryptSentence(inputSentence) {
-  let encryptedArray = [...inputSentence].map(letter => {
-    return (letter === ' ') ? (encryptedLetter = '-') : (encryptedLetter = '_');
+function encryptProverb() {
+  let encryptedArray = [...state.proverb].map(letter => {
+    if (state.correct.indexOf(letter) === -1){
+      return (letter === ' ') ? (encryptedLetter = '-') : (encryptedLetter = '_');
+    } else {
+      return letter;
+    }
   })
   return encryptedArray.join('');
 }
 
 function letterCheck(clickedLetter) {
   state.roundCounter++;
-  const letterPositions = charPositions(clickedLetter, state.proverb);
-  let currentDisplayedProverb = state.displayedProverb;
-  letterPositions.forEach(item => currentDisplayedProverb = currentDisplayedProverb.substr(0, item) + clickedLetter + currentDisplayedProverb.substr(item + 1));
-  state.displayedProverb = currentDisplayedProverb;
-  sentence.innerHTML = state.displayedProverb;
-}
-
-function charPositions(letter, proverb){
-  var positions=[],i=-1;
-  while((i=proverb.indexOf(letter,i+1)) >= 0) positions.push(i);
-  return positions;
+  (state.proverb.includes(clickedLetter) === true) ? (state.correct.push(clickedLetter)) : (state.wrong.push(clickedLetter));
+  displayProgress();
 }
